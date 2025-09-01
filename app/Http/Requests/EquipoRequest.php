7f09705a02653c00ff75da39a5a077895a6ecdc3
Cristian Solano <cfsv2018@gmail.com>
2025-09-01 11:59:19 -0500

    Messages

--- app/Http/Requests/EquipoRequest.php
+++ app/Http/Requests/EquipoRequest.php
@@ -25,8 +25,31 @@ class EquipoRequest extends FormRequest
             'nombre_equipo' => 'required | string | max:255 | min:3' ,
             'ciudad_equipo' => 'required | string | max:255 | min:3',
             'pais_equipo' => 'required | string | max:255 | min:3', 
-            'fecha_fundacion_equipo' => 'required | date | before:2025-09-26',
+            'fecha_fundacion_equipo' => 'required | date | before:today',
             'liga_equipo' => 'required | string | max:255 | min:3'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'nombre_equipo.required' => 'El nombre es obligatorio',
+            'nombre_equipo.string' => 'El nombre debe ser una cadena de texto',
+            'nombre_equipo.max' => 'El nombre no puede tener más de 255 caracteres',
+            'nombre_equipo.min' => 'El nombre debe tener al menos 3 caracteres',
+            'ciudad_equipo.string' => 'La ciudad debe ser una cadena de texto',
+            'ciudad_equipo.max' => 'La ciudad no puede tener más de 255 caracteres',
+            'ciudad_equipo.required' => 'La ciudad es obligatoria',
+            'ciudad_equipo.min' => 'La ciudad debe tener al menos 3 caracteres',
+            'pais_equipo.string' => 'El país debe ser una cadena de texto',
+            'pais_equipo.required' => 'El país es obligatorio',
+            'pais_equipo.max' => 'El país no puede tener más de 255 caracteres',
+            'pais_equipo.min' => 'El país debe tener al menos 3 caracteres',
+            'fecha_fundacion_equipo.required' => 'La fecha de fundación es obligatoria',
+            'liga_equipo.required' => 'La liga del equipo es requerida',
+            'liga_equipo.string' => 'La liga del equipo debe ser una cadena de texto',
+            'liga_equipo.max' => 'La liga del equipo no puede tener más de 255 caracteres',
+            'liga_equipo.min' => 'La liga del equipo debe tener al menos 3 caracteres',
         ];
     }
 }
